1a9258ec6e3e7c70297228431c222e7b21a058d0
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-02-21 23:10:53 -0300

    continuando com a estilização da tela de criar a tarefa

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -22,14 +22,14 @@ export default function Task(){
     return(
         <KeyboardAvoidingView style={styles.container}>
             <Header showBack={true}/>
-
             <ScrollView style={{width:'100%'}}>
 
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginVertical: 10}}>
                     {
                         typeIcons.map((icon) =>(
+                            icon != null &&
                             <TouchableOpacity>
-                                <Image key={icon} source={icon} style={styles.imageIcons}/>
+                                <Image key={icon} source={icon} style={styles.imageIcon}/>
                             </TouchableOpacity>
                         ))
                     }
@@ -38,11 +38,21 @@ export default function Task(){
                 <Text style={styles.label}>Título</Text>
                 <TextInput style={styles.input} maxLength={30} placeholder={"Lembre-me de fazer..."}/>
 
-                <Text style={styles.label}>Título</Text>
-                <TextInput style={styles.input} maxLength={30} placeholder={"Lembre-me de fazer..."}/>
+                <Text style={styles.label}>Detalhes</Text>
+                <TextInput style={styles.input} multiline={true} maxLength={200} placeholder={"Detalhes da atividade que eu tenho que lembrar..."}/>
+
+                <View style={styles.inLine}>
+                    <View style={styles.inputInLine}>
+                        <Switch/>
+                        <Text style={styles.switchLabel}>Concluído</Text>
+                    </View>
+
+                    <TouchableOpacity>
+                        <Text style={styles.removeLabel}>EXCLUIR</Text>
+                    </TouchableOpacity>
+                </View>
 
             </ScrollView>
-
             <Footer/>
         </KeyboardAvoidingView>
     )
--- src/views/Task/style.js
+++ src/views/Task/style.js
@@ -5,6 +5,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         height: "100%",
+    },
+    imageIcon:{
+        width: 40,
+        height: 40,
+        marginHorizontal: 10,
+    },
+    label:{
+        color: '#707070',
+        fontSize: 16,
+        paddingHorizontal: 10,
+        marginTop: 20,
+        marginBottom: 5,
+    },
+    input:{
+        fontSize: 16,
+        padding: 10,
     }
 })
 
