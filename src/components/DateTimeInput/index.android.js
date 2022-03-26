fbec731a15ff6347ec1e61a967bf01b409b06b2f
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-03-25 22:27:13 -0300

    Resolvendo erros de fuso horario

--- src/components/DateTimeInput/index.android.js
+++ src/components/DateTimeInput/index.android.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from "react";
-import DateTimePicker, {RNDateTimePicker} from '@react-native-community/datetimepicker'; //Importando DateTimePicker que é responsável por capturar a data e hora
+import DateTimePicker from '@react-native-community/datetimepicker'; //Importando DateTimePicker que é responsável por capturar a data e hora
 import format from "date-fns/format"; //Importando função para formatar datas
-import { isPast} from 'date-fns';
+import { isPast, getMonth, getDate, getYear, getHours, getMinutes} from 'date-fns';
 import {
     TouchableOpacity,
     Image,
@@ -16,24 +16,31 @@ import iconCalendar from '../../assets/calendar.png';
 import iconClock from '../../assets/clock.png';
 
 
+
 export default function DateTimeInputAndroid({type, save, calendar, hour}){
     const [date, setDate] = useState(new Date()); //Criando variável de estado que recebe um objeto do tipo data que pega a data e hora atual
     const [mode, setMode] = useState('date'); //Criando variável de estado que recebe um valor 'date' como padrão
     const [show, setShow] = useState(false); //Criando variável de estado que determina se mostra ou não o calendário ou relógio pro usuário escolher
-    
+
 
     const onChange = (event, selectedDate) => { //Função que serve para atribuir nas váriaveis criadas a data e hora e se mostra ou esconde o calendário e o relógio
-      
-      
-        
         const currentDate = selectedDate || date; //Criando constante que recebe: ou a data e hora selecionadas pelo usuário, ou a data e hora atual
-        
 
-        setShow(false); //Quando o usuario escolher a data ou hora, então o componente que seleciona a data e hora não será mais exibido
+        //Data do dia escolhido
+        const month = getMonth(currentDate);
+        const day = getDate(currentDate);
+        const year = getYear(currentDate);
+        const hours = getHours(currentDate);
+        const minutes = getMinutes(currentDate);
+        const dataEcolhida = new Date(year, month, day, hours, minutes, 59); //Montando uma nova data atual com a data escolhida
 
-          console.log(currentDate);
+        if (isPast(dataEcolhida)) {//Se a data escolhida estiver no passado
+          Alert.alert("Você não pode escolher uma data no passado");
+          setShow(false); //Quando o usuario escolher a data ou hora, então o componente que seleciona a data e hora não será mais exibido
+          setDate(new Date());
 
-
+        }else{//Se a data escolhida não estiver no passado
+          setShow(false); //Quando o usuario escolher a data ou hora, então o componente que seleciona a data e hora não será mais exibido
           setDate(currentDate); //Atribuindo a constante "currentDate" a variável de estado "date" para determinar a data e hora
           //setShow(Platform.OS === 'ios'); //Verificando qual a plataforma do que o app está rodando
 
@@ -43,7 +50,7 @@ export default function DateTimeInputAndroid({type, save, calendar, hour}){
           } else {
             save(format(currentDate, 'HH:mm:ss')); //Modificando o hour para ficar no formato do MongoDB
           }
-
+        }
     };
 
 
