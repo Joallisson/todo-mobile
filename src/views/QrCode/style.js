02db3343b9dfe9b867fd1fd35791c93eca84895b
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-04-03 16:21:08 -0300

    Resolvendo Bugs da tela do scanner do qrcode

--- src/views/QrCode/style.js
+++ src/views/QrCode/style.js
@@ -2,9 +2,8 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        height: '100%',
         flexDirection: 'column',
-        
     },
     header: {
         width: '100%',
